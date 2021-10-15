ef14f62e248f5002d30259f6d86345942853284b
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2021-10-15 23:30:10 +0100

    chaper 1 wip

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+        }
+    }
+}
